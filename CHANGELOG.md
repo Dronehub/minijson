1c285409d35b950fb7d2d6fb9bf6fc9ac31c642c
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-05-26 16:47:39 +0200

    ver bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,5 @@
 Changelog is kept at [GitHub](https://github.com/Dronehub/minijson/releases),
 here's only the changelog for the version in development
 
-# v1.6
+# v1.7
 
-* improved checking for malformed structures during parse
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if 'DEBUG' in os.environ:
     directives['embedsignature'] = True
 
 
-setup(version='1.6',
+setup(version='1.7a1',
       packages=find_packages(include=['minijson', 'minijson.*']),
       ext_modules=build([Multibuild('minijson', find_pyx('minijson'),
                                     dont_snakehouse=dont_snakehouse), ],
