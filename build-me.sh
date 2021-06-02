a2864cf8c99c488b5e33441d81bcdafb5ab84aa7
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-06-02 19:34:41 +0200

    fix build script

--- build-me.sh
+++ build-me.sh
@@ -7,10 +7,8 @@
 apt-get update
 apt-get install -y patchelf
 pip install auditwheel doctor-wheel twine cython
-git clone https://github.com/Dronehub/minijson.git
-cd minijson
 python setup.py bdist_wheel
 cd dist
 doctor-wheel "*.whl"
 auditwheel repair --plat "manylinux2014_$(uname -m)" "*.whl"
-twine upload "wheelhouse/*.whl"
+twine upload -u $PYPI_USER -p $PYPI_PWD "wheelhouse/*.whl"
